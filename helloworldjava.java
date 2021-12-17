32126ed5b62cae952e33cb1341745f2351de4e53
rbsaranya <96078773+rbsaranya@users.noreply.github.com>
2021-12-17 18:48:02 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,7 @@
 public class helloworldjava {
    public static void main(String[] args) {
     System.out.println("Hello World from DevOps - poll test");
+      System.out.println("Hello World from DevOps - webhook test");
     }
 }
 
